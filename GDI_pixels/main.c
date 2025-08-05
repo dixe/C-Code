11608cdc42e63e580d4b9642acf52fdfbf274c6c
Nikolaj Østergaard <niko.fris@gmail.com>
2025-08-05 22:29:46 +0200

    Pass quit and redraw functions to draw window

--- GDI_pixels/main.c
+++ GDI_pixels/main.c
@@ -8,8 +8,8 @@
 
 // based on code from https://www.youtube.com/watch?v=q1fMa8Hufmg
 
-static bool quit = false;
 
+static bool quit = false;
 
 COLORREF getCusorColor()
 {
@@ -24,11 +24,16 @@ COLORREF getCusorColor()
 
 static int reduce = 10000;
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, int nCmdShow) {
+void VmPaint(HWND window_handle);
+void OnQuit() {
+  quit = true;
+}
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, int nCmdShow) {
 
   // WINDOW SETUP
   static HWND window_handle;
-  window_handle = NewWindow(hInstance);
+  window_handle = NewWindow(hInstance, OnQuit, VmPaint);
 
   while (!quit) {
     static MSG message = { 0 };
@@ -55,83 +60,41 @@ void DrawCursorColor(HDC hdc) {
 }
 
 
-LRESULT CALLBACK WindowProcessMessage(HWND window_handle, UINT message, WPARAM wParam, LPARAM lParam) {
 
-  PAINTSTRUCT paint;
-  HDC hdc;
+void VmPaint(HWND window_handle) {
+  PAINTSTRUCT ps;
+  HDC hdc = BeginPaint(window_handle, &ps);
+  // Create a memory DC compatible with the screen
+  HDC memDC = CreateCompatibleDC(hdc);
+  RECT clientRect;
+  GetClientRect(window_handle, &clientRect);
 
-  switch (message) {
-  case WM_QUIT:
-  case WM_DESTROY: {
-    quit = true;
-  } break;
+  // Create a bitmap compatible with the window DC
+  HBITMAP memBitmap = CreateCompatibleBitmap(hdc, clientRect.right, clientRect.bottom);
+  HBITMAP oldBitmap = (HBITMAP)SelectObject(memDC, memBitmap);
 
-  case WM_PAINT:
+  // Fill background
+  HBRUSH bgBrush = (HBRUSH)(COLOR_WINDOW + 1);
+  FillRect(memDC, &clientRect, bgBrush);
+
+  // Draw text into the memory DC
+  if (reduce > 0)
   {
-    PAINTSTRUCT ps;
-    HDC hdc = BeginPaint(window_handle, &ps);
-    // Create a memory DC compatible with the screen
-    HDC memDC = CreateCompatibleDC(hdc);
-    RECT clientRect;
-    GetClientRect(window_handle, &clientRect);
-
-    // Create a bitmap compatible with the window DC
-    HBITMAP memBitmap = CreateCompatibleBitmap(hdc, clientRect.right, clientRect.bottom);
-    HBITMAP oldBitmap = (HBITMAP)SelectObject(memDC, memBitmap);
-
-    // Fill background
-    HBRUSH bgBrush = (HBRUSH)(COLOR_WINDOW + 1);
-    FillRect(memDC, &clientRect, bgBrush);
-
-    // Draw text into the memory DC
-    if (reduce > 0)
-    {
-      TextOutW(memDC, 0, 0, L"Hello, Windows !", 15);
-    }
-    else
-    {
-      TextOutW(memDC, 0, 0, L"JHello !", 8); // Correct length
-    }
-
-    // Blit the memory DC to the screen
-    BitBlt(hdc, 0, 0, clientRect.right, clientRect.bottom, memDC, 0, 0, SRCCOPY);
-
-    // Clean up
-    SelectObject(memDC, oldBitmap);
-    DeleteObject(memBitmap);
-    DeleteDC(memDC);
-
-    EndPaint(window_handle, &ps);
-
-  } break;
-    /*
-  case WM_PAINT: {
-    static PAINTSTRUCT paint;
-    static HDC device_context;
-    device_context = BeginPaint(window_handle, &paint);
-
-    Graphics graphics(device_context);
-
-    // draw rect to clear screen
-    RECT window_rect;
-    GetWindowRect(window_handle, &window_rect);
-    
-    //Rectangle(hDC, window_rect.left, window_rect.top, window_rect.right, window_rect.bottom);
-
-    FillRect(device_context, &window_rect, (HBRUSH)(COLOR_WINDOW + 1));
-
-   
-
-    DrawCursorColor(device_context);   
-
-
-    EndPaint(window_handle, &paint);
-  } break;
-  */
-
-  default: {
-    return DefWindowProc(window_handle, message, wParam, lParam);
+    TextOutW(memDC, 0, 0, L"Hello, Windows !", 15);
   }
+  else
+  {
+    TextOutW(memDC, 0, 0, L"JHello !", 8); // Correct length
   }
-  return 0;
+
+  // Blit the memory DC to the screen
+  BitBlt(hdc, 0, 0, clientRect.right, clientRect.bottom, memDC, 0, 0, SRCCOPY);
+
+  // Clean up
+  SelectObject(memDC, oldBitmap);
+  DeleteObject(memBitmap);
+  DeleteDC(memDC);
+
+  EndPaint(window_handle, &ps);
 }
+
--- GDI_pixels/window.c
+++ GDI_pixels/window.c
@@ -1,4 +1,3 @@
-
 #include "window.h"
 
 
@@ -9,7 +8,15 @@ LRESULT CALLBACK WindowProcessMessage(HWND, UINT, WPARAM, LPARAM);
 #define Rand32() rand()
 #endif
 
-HWND NewWindow(HINSTANCE hInstance) {
+
+typedef void (*quit_fn) ();
+typedef void (*draw_fn) ();
+
+quit_fn quit_f;
+draw_fn draw_f;
+ 
+
+HWND NewWindow(HINSTANCE hInstance, quit_fn qf, draw_fn df) {
   // WINDOW SETUP
   const wchar_t window_class_name[] = L"My Window Class";
   static WNDCLASS window_class = { 0 };
@@ -18,6 +25,8 @@ HWND NewWindow(HINSTANCE hInstance) {
   window_class.lpszClassName = window_class_name;
   RegisterClass(&window_class);
 
+  quit_f = qf;
+  draw_f = df;
 
   static HWND window_handle;
   window_handle = CreateWindow(window_class_name, L"Drawing Pixels", WS_OVERLAPPEDWINDOW | WS_VISIBLE,
@@ -25,4 +34,52 @@ HWND NewWindow(HINSTANCE hInstance) {
   if (window_handle == NULL) { return window_handle; }
 
   return window_handle;
+}
+
+
+LRESULT CALLBACK WindowProcessMessage(HWND window_handle, UINT message, WPARAM wParam, LPARAM lParam) {
+
+  PAINTSTRUCT paint;
+  HDC hdc;
+
+  switch (message) {
+  case WM_QUIT:
+  case WM_DESTROY: {
+    quit_f();
+  } break;
+
+  case WM_PAINT:
+  {
+    draw_f(window_handle);
+  } break;
+  /*
+case WM_PAINT: {
+  static PAINTSTRUCT paint;
+  static HDC device_context;
+  device_context = BeginPaint(window_handle, &paint);
+
+  Graphics graphics(device_context);
+
+  // draw rect to clear screen
+  RECT window_rect;
+  GetWindowRect(window_handle, &window_rect);
+
+  //Rectangle(hDC, window_rect.left, window_rect.top, window_rect.right, window_rect.bottom);
+
+  FillRect(device_context, &window_rect, (HBRUSH)(COLOR_WINDOW + 1));
+
+
+
+  DrawCursorColor(device_context);
+
+
+  EndPaint(window_handle, &paint);
+} break;
+*/
+
+  default: {
+    return DefWindowProc(window_handle, message, wParam, lParam);
+  }
+  }
+  return 0;
 }
